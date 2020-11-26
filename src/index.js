0cddfa76eccefab24a0766005beba1c79b668b07
dev-jv <dev.javierleon@gmail.com>
2020-11-26 16:31:38 -0500

    eight

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import PrimeraApp from './PrimeraApp';
 
+// import './index.css';
+
+// const divRoot = document.querySelector('#app');
+
+// ReactDOM.render( <PrimeraApp />, divRoot);
+
+//---------------------------------------- <> Test
+import React from 'react';
+import CounterApp from './CounterApp';
+import ReactDOM from 'react-dom'
 import './index.css';
+
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render( <PrimeraApp />, divRoot);
+ReactDOM.render(<CounterApp value = {1} />, divRoot);
 
 
 
