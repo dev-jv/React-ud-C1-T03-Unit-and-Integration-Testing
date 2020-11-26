5d1a58c4096ab418365e748397f79d51b3df2358
dev-jv <dev.javierleon@gmail.com>
2020-11-26 15:43:45 -0500

    seven

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PrimeraApp = ({ color }) => {
-
-    // if ( !color ) {
-    //     throw new Error( 'El color no existe!' )
-    // }
-
+const PrimeraApp = ({ color = 'Yellow', especie }) => {
+    
     return (
         <>  
             <h1> { color } </h1>
-            {/* <pre>{JSON.stringify(hope, null, 3)}</pre> */}
-            <p>My first App</p>  
+            <p> { especie } </p>
         </>
     );
 }
 
-PrimeraApp.propTypes = {
+PrimeraApp.propTypes = { 
     color : PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+    especie: 'Kaede',
 }
 
 export default PrimeraApp;
