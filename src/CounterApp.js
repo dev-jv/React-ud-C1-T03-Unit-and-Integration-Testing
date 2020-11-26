29674dd3e80a49ebae8e5fbeb51059c0bee3127e
dev-jv <dev.javierleon@gmail.com>
2020-11-26 18:06:03 -0500

    ten

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
 
-    const handleAdd = (e) => {
-        console.log(e);
+    const handleAdd = () => {
+        // setCounter( counter + 1 );
+        setCounter( (c) => c + 1 );
     }
 
+    const [ counter, setCounter ] = useState( 0 );
+
     return (
-        <>
+        <> 
             <h1> CounterApp </h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd }> +1 </button>
-        </>
+        </> 
     );
 }
 
@@ -24,3 +27,4 @@ export default CounterApp;
 
 
 
+ 
