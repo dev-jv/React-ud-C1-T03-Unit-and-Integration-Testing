ba228020a14d8ad2c96b4a3fed05074b8d65517f
dev-jv <dev.javierleon@gmail.com>
2020-11-26 10:22:28 -0500

    three

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,41 @@
+// ----------------------------- <> ...
+// import React from 'react';
+
+// const PrimeraApp = () => {
+//     return (
+//         <div>
+//             <h1>Wi</h1>
+//             <p>My first App</p>  
+//         </div>
+//     );
+// }
+
+// export default PrimeraApp;
+
+// ----------------------------- <> with
+// import React, { Fragment } from 'react';
+
+// const PrimeraApp = () => {
+//     return (
+//         <Fragment>
+//             <h1>Wi</h1>
+//             <p>My first App</p>  
+//         </Fragment>
+//     );
+// }
+
+// export default PrimeraApp;
+
+// ----------------------------- <> without
 import React from 'react';
 
 const PrimeraApp = () => {
-    return <h1>Wi</h1>
+    return (
+        <>
+            <h1>Wi</h1>
+            <p>My first App</p>  
+        </>
+    );
 }
 
 export default PrimeraApp;
