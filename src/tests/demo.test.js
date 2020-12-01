66425a483177888669ea2446a9434220d6a400b7
dev-jv <dev.javierleon@gmail.com>
2020-12-01 11:16:50 -0500

    thirteen

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,8 +1,17 @@
 
+describe('Pruebas en el archivo dem.test.js', () => {
+    test ( 'Deben ser iguales los string', () => {
+    
+        // 1. inicialización
+        const mensaje = 'Hope';
+    
+        // 2. estímulo
+        const mensaje2 = `Hope`;
+    
+        // 3. Observar el comportamiento
+        expect( mensaje ).toBe( mensaje2 );
+    });
+});
 
-test ( 'Debe ser true', () => {
-    const isActive = true;
-    if ( isActive ) {
-        throw new Error('No está activo')
-    }
-} )
+
+
