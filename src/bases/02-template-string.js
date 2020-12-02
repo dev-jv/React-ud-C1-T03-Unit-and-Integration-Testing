42ce34cd9f7e47ac4bf11df95749c246d73b0907
dev-jv <dev.javierleon@gmail.com>
2020-12-02 18:07:54 -0500

    fourteen

--- src/bases/02-template-string.js
+++ src/bases/02-template-string.js
@@ -4,25 +4,25 @@ const nombre = 'Harry';
 const apellido = 'Potter';
 
 const nS = nombre + ' ' + apellido;
-console.log(nS);
+// console.log(nS);
 
 const nC = `${nombre} ${apellido}`;
-console.log(nC);
+// console.log(nC);
 
 const nombreCompleto = `
 ${nombre} 
 ${apellido} 
 ${1+1}`
 ;
-console.log(nombreCompleto);
+// console.log(nombreCompleto);
 
-function getSaludo(ka) {
-    return 'Bienvenido ' + ka;
+export function getSaludo(ka = 'Nero') {
+    return 'Bienvenido ' + ka + '!';
 }
 
-console.log(`Este es un texto: ${getSaludo(nombre)}`);
+// console.log(`Este es un texto: ${getSaludo(nombre)}`);
 
-console.log(`Este es un texto: ${getSaludo(nC)}`);
+// console.log(`Este es un texto: ${getSaludo(nC)}`);
 
 
 
---
+++ src/tests/bases/02-template-string.test.js
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom'
+
+import { getSaludo } from '../../bases/02-template-string.js'
+
+describe('Pruebas en 02-template-string.js', () => {
+    
+    test('Test 1: getSaludo, con parámetros', () => {
+        const nombre = 'Nevile';
+        const saludo = getSaludo(nombre);
+        console.log(saludo);
+        expect(saludo).toBe('Bienvenido ' + nombre + '!');
+    })
+
+    test('Test 2: getSaludo, con parámetros por defecto ', (nombre = 'Nero') => {
+        const saludo = getSaludo();
+        console.log(saludo);
+        expect(saludo).toBe('Bienvenido ' + nombre + '!');
+    })
+})
+
+
+
