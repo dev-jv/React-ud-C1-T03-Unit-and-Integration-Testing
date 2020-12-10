1509e79debddd8fbd91f64a478368551ab73f21a
dev-jv <dev.javierleon@gmail.com>
2020-12-10 16:49:13 -0500

    seventeen

--- src/bases/08-exp-imp-func.js
+++ src/bases/08-exp-imp-func.js
@@ -1,60 +1,66 @@
 // ------------------------------------------- <> Import, Export ...
 
-import heroes, { owners } from './data/heroes'
+import heroes, { owners } from '../data/heroes'
 
 
-const [ , , , k, ,] = heroes; 
+// const [ , , , k, ,] = heroes; 
 
-console.log( k );
+// console.log( k );
 
-console.log( k.name );
+// console.log( k.name );
 
-// ------------------------------------------ 
-const getById = (id) => {
-    return heroes.find( ( heroe ) => {
-        return true; // devuelve el elemento de indice id..
-    });
-};
+// // ------------------------------------------ 
+// const getById = (id) => {
+//     return heroes.find( ( heroe ) => {
+//         return true; // devuelve el elemento de indice id..
+//     });
+// };
 
-console.log( getById(2) );
+// console.log( getById(2) );
 
-// ------------------------------------------ 
-const getHeroById = (id) => {
-    return heroes.find( ( heroe ) => {
-        if ( heroe.id === id){
-            return true;
-        } else {
-            return false;
-        }
-    });
-};
+// // ------------------------------------------ 
+// const getHeroById = (id) => {
+//     return heroes.find( ( heroe ) => {
+//         if ( heroe.id === id){
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     });
+// };
 
-console.log( getHeroById(2) );
+// console.log( getHeroById(2) );
 
-// ------------------------------------------ 
-const getHeroeById = (id) => {
-    return heroes.find(heroe => heroe.id === id); // Al ser una igualdad la evalua como true/false
-}
+// // ------------------------------------------ 
+// const getHeroeById = (id) => {
+//     return heroes.find(heroe => heroe.id === id); // Al ser una igualdad la evalua como true/false
+// }
 
-console.log( getHeroeById(2) );
+// console.log( getHeroeById(2) );
 
 // ------------------------------------------ 
 const getByIdH = id => heroes.find(heroe => heroe.id === id);
 
-console.log( getByIdH(3) );
+// console.log( getByIdH(3) );
 
 // ------------------------------------------ 
 const getByOwnDC = owner => heroes.filter(heroe => heroe.owner === owner);
 
-console.log( getByOwnDC('DC') );
+// console.log( getByOwnDC('DC') );
 
 // ------------------------------------------ 
 const getByOwnM = x => heroes.filter(e => e.owner === x); // según x...nuestro owner
 
-console.log( getByOwnM('Marvel') );
+// console.log( getByOwnM('Marvel') );
 
-// ------------------------------------------ 
-console.log(owners);
+// // ------------------------------------------ 
+// console.log(owners);
+
+export {
+    getByIdH,
+    getByOwnM,
+    getByOwnDC
+}
 
 
 
---
+++ src/tests/bases/08-exp-imp-func.test.js
@@ -0,0 +1,34 @@
+import { getByIdH, getByOwnDC, getByOwnM } from '../../bases/08-exp-imp-func';
+
+import heroes from '../../data/heroes';
+
+describe('Pruebas en funciones de Héroes', () => {
+    test('Debe de retornar un heroe por id', () => {
+        const id = 5;
+        const heroId = getByIdH(id)
+        const heroIdTest = heroes.find(heroe => heroe.id === id)
+        expect(heroId).toEqual(heroIdTest);
+    })
+    test('Debe retornar undefined si el heroe no existe', () => {
+        const id = 6;
+        const heroId = getByIdH(id)
+        expect(heroId).toBe(undefined);
+    })
+    test('Debe retornar un array con los heroes de DC', () => {
+        const owner = 'DC';
+        const heroDC = getByOwnDC(owner);
+        // console.log(heroDC);
+        const heroDCTest = heroes.filter(heroe => heroe.owner === owner);
+        // console.log(heroDCTest);
+        expect(heroDC).toEqual(heroDCTest);
+    })
+    test('Debe retornar un array con los heroes de Marvel', () => {
+        const owner = 'Marvel'; 
+        const heroMar = getByOwnM(owner).length;
+        // console.log(heroMar);
+        expect(heroMar).toBe(2);
+    })
+})
+
+
+
